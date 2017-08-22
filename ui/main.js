3d9b17571f18e8be492757d46d45a59af7bd734f
Rohan Gupta <gupta.rohan1711@gmail.com>
2017-08-22 20:11:34 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,93 @@
-var button=document.getElementById('counter');
-button.onClick=function(){
-    alert("sef");
+
+//counter code
+var button=document.getElementById("b1");
+
+
+button.onclick = function()
+{
+	//create a request object
+	var request=new XMLHttpRequest();
+
+	//capture the response and store in a variable
+	request.onreadystatechange=function()
+	{
+
+		if(request.readyState===XMLHttpRequest.DONE)
+		{
+
+			//take some action
+			if(request.status===200)
+			{
+
+				var counter=request.responseText;
+				 document.getElementById("s1").innerHTML = counter.toString();
+			}
+		}
+
+
+	}
+
+//make request to the server
+request.open('GET','/counter',true);
+request.send(null);
+
+
 }
+
+
+
+
+
+//submit name
+
+
+var submit=document.getElementById('submit_btn');
+
+submit.onclick=function()
+{
+	//make a request to the server and send the name
+	//capture a lisr of name and render it as list
+
+	var nameInput=document.getElementById('name');
+	var name=nameInput.value;
+
+	var request=new XMLHttpRequest();
+
+	//capture the response and store in a variable
+	request.onreadystatechange=function()
+	{
+
+		if(request.readyState===XMLHttpRequest.DONE)
+		{
+
+			//take some action
+			if(request.status===200)
+			{
+
+				var names=request.responseText;
+				names=JSON.parse(names);
+
+				var list='';
+				for(var i=0;i<names.length;i++)
+				{
+					list+='<li>'+names[i]+'</li>';
+				}
+
+				var ul=document.getElementById('namelist');
+				ul.innerHTML=list;
+							
+			}
+		}
+
+
+	}
+
+	request.open('GET','/submit-name?name='+name,true);
+	request.send(null);
+
+
+}
+
+
+
+
