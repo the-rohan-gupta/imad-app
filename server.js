b5cc8bb982e953c2267f75f50630ce941c04f4bf
Rohan Gupta <gupta.rohan1711@gmail.com>
2017-08-27 21:03:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get('/test-db', function (req, res)
         }
         else
         {
-            res.send(JSON.stringify(result));
+            res.send(JSON.stringify(result.rows));
         }
     });
 });
