f48ce8695decc6a0e29902f1f69d21c2cf77cb7a
Rohan Gupta <gupta.rohan1711@gmail.com>
2017-08-27 20:44:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var config=
     database: 'guptarohan1711' ,
     host: 'db.imad.hasura-app.io' ,
     port: '5432' ,
-    password:process.env.DB_PASSWORD
+    password:process.env.DB_PASSWORD ,
 };
 
 
