2d36aa3afc5849037fd063f95ccf3936ab47945b
Rohan Gupta <gupta.rohan1711@gmail.com>
2017-08-29 00:06:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -118,7 +118,7 @@ app.get('/articles/:articleName',function(req,res)
 function hash(input,salt)
 {
     var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
-    return hashed;
+    return hashed.toString('hex');
 }
 
 
