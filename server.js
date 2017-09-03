a95dc288612aec42871fa6eab48b307dd0d183c8
Rohan Gupta <gupta.rohan1711@gmail.com>
2017-09-03 19:22:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -176,7 +176,7 @@ app.post('/login',function(req,res)
        }
        else
        {
-          /* var dbString=result.row[0].password;
+           var dbString=result.row[0].password;
            var salt=dbString.split('$')[2];
            var hashPassword=hash(password,salt);
            
@@ -187,9 +187,7 @@ app.post('/login',function(req,res)
            else
            {
                res.send('invalid');
-           }*/
-           var x=1;
-                          res.send(x.toString());
+           }
        }
    });
 });
